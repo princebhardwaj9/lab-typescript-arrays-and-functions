90afb751a182232a0a999f4065a7cb611d17cc21
princebhardwaj9 <princebhardwaj.110@gmail.com>
2020-04-22 00:18:47 +0530

    FSJ0320A021

--- app-service.ts
+++ app-service.ts
@@ -1,7 +1,7 @@
 export interface ArrayService {
+  arraySort(myArray : any) : Array<string>;
   arrayMultiply(myArray : any) : Array<Number>;
+  arrayReplace(myArray : any) : Array<Number>;
   arraySeparate(myArray : any) : Array<string>;
   arraySplit(myString : string) : Array<number>;
-  arraySort(myArray : any) : Array<string>;
-  arrayReplace(myArray : any) : Array<Number>;
 }
